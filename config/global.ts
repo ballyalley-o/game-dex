be4b354a132c9200167416dba1c4120a5fca57c0
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-03 01:29:26 +1200

    Update API_URL configuration to use environment variable
    This commit updates the `config/global.ts` file to use the `API_URL` environment variable for the API URL configuration. This allows for greater flexibility and easier configuration management. The previous hard-coded value has been removed.

--- config/global.ts
+++ config/global.ts
@@ -1,9 +1,13 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export const GLOBAL = {
   API_NAME: 'NBA api',
   API_PORT: process.env.API_PORT || 3005,
-  API_URL: process.env.API_URL,
   API_VERSION: process.env.API_VERSION || '',
+  API_URL: process.env.API_URL,
   //  :database
+  DB_URI: process.env.DB_URI,
   // :env
   ENV: process.env.ENV || 'development'
 }

5bb09ee9e07007f48949eeb375c673a9d25f9cb5
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-02 23:59:08 +1200

    feat: Add typings for global and module declarations
    This commit adds typings for global and module declarations. The global declarations include `IConNex` and `Env` interfaces. The module declarations import `Application` from the `express` module and include a commented out code block for extending the `Application` interface.
    
    Recent user commit: feat: Add main route to link API route to the application

---
+++ typings/global.d.ts
@@ -0,0 +1,8 @@
+declare interface IConNex {
+  (...params: any[]): string
+}
+
+declare interface Env {
+  PRODUCTION: 'production'
+  DEVELOPMENT: 'development'
+}
---
+++ typings/module.d.ts
@@ -0,0 +1,7 @@
+import { Application } from 'express'
+
+// declare module "express" {
+//     interface Application {
+//         linkApiRoute(): void
+//     }
+// }

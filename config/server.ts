419ad6f970ce7236638e44fa88e8f2f6bd549080
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-03 01:29:54 +1200

    Add morgan logging middleware for request logging
    This commit adds the `morgan` middleware to the server configuration in `config/server.ts`. The `morgan` middleware is used for request logging and provides detailed information about incoming requests. This improves the visibility and debugging capabilities of the application.
    
    Recent user commit: feat: Add main route to link API route to the application

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
 import express, { Application } from 'express'
 import goodlog from 'good-logs'
-import { LogInitRequest } from 'decorator'
+import morgan from 'morgan'
+import { LogInitRequest, ConnectionStatus } from 'decorator'
 import { mainRoute } from 'route'
+import { AppRouter } from 'app-router'
 import { GLOBAL } from 'config/global'
 import { KEY } from 'constant'
 import dotenv from 'dotenv'
@@ -12,34 +14,43 @@ const ENV = GLOBAL.ENV
 
 class App {
   private _app: Application
+  isConnected: boolean = false
 
   static app() {
     const app = new App()
+    app.start()
   }
 
   constructor() {
     this._app = express()
     this._app.use(express.json())
+    this._app.use(morgan('combined'))
     this.registerRoute()
   }
 
   @LogInitRequest
   private registerRoute() {
+    this._app.use(AppRouter.instance)
+    AppRouter.serverRouter()
     mainRoute(this._app)
   }
 
+  @ConnectionStatus
   public start() {
     let _ENV = ENV === KEY.DEVELOPMENT ? ('DEVELOPMENT' as keyof Env) : ('PRODUCTION' as keyof Env)
 
     try {
-      this._app.listen(() => {
-        goodlog.server(PORT, GLOBAL.API_VERSION, _ENV, true)
+      this._app.listen(PORT, () => {
+        goodlog.server(PORT, GLOBAL.API_VERSION, _ENV, this.isConnected)
       })
     } catch (error: any) {
       process.on(KEY.UNHANDLED_REJECTION, (err) => {
-        goodlog.server(PORT, GLOBAL.API_VERSION, _ENV, false)
+        goodlog.server(PORT, GLOBAL.API_VERSION, _ENV, this.isConnected)
         goodlog.error(error.message)
+        this.isConnected = false
       })
     }
   }
 }
+
+export default App

5c677ac972b771d73c942f723ff179fc83187c91
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-04 07:36:04 +1200

    Add ASSIST_LEADER and SCORE_LEADER to KEY enum
    The code changes in `constant/key.ts` add two new values, `ASSIST_LEADER` and `SCORE_LEADER`, to the `KEY` enum. This enhancement improves the codebase by providing additional keys for specific purposes related to leaderboards or statistics.

--- constant/key.ts
+++ constant/key.ts
@@ -1,5 +1,7 @@
 export enum KEY {
   UNHANDLED_REJECTION = 'unhandledRejection',
   PRODUCTION = 'production',
-  DEVELOPMENT = 'development'
+  DEVELOPMENT = 'development',
+  ASSIST_LEADER = 'AssistLeader',
+  SCORE_LEADER = 'ScoreLeader'
 }

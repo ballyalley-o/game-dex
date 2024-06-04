cfb72350abda3fe904a4003b2e236e4f17927ad8
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-05 01:19:26 +1200

    Add models for NBA game, stats, player stats, team stats, and game stats
    This commit adds new models for NBA game, stats, player stats, team stats, and game stats. These models provide a structured representation of the data related to NBA games, player statistics, team statistics, and game statistics. The addition of these models enhances the codebase by improving the organization and readability of the code, and enables better handling and manipulation of NBA-related data.

--- typings/model.d.ts
+++ typings/model.d.ts
@@ -23,6 +23,57 @@ declare interface AllTimeAssistLeaders {
   [key: string]: any
 }
 
+declare interface Game {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  date: Date
+  season: Schema.Types.ObjectId
+  homeTeam: Schema.Types.ObjectId
+  awayTeam: Schema.Types.ObjectId
+  homeScore: number
+  awayScore: number
+  winner: Schema.Types.ObjectId
+  stats: Schema.Types.ObjectId
+  [key: string]: any
+}
+
+declare interface Stats {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  minutes: number
+  fieldGoalsMade: number
+  fieldGoalsAttempted: number
+  fieldGoalPercentage: number
+  threePointMade: number
+  threePointAttempted: number
+  threePointPercentage: number
+  freeThrowsMade: number
+  freeThrowsAttempted: number
+  freeThrowPercentage: number
+  offensiveRebounds: number
+  defensiveRebounds: number
+  totalRebounds: number
+  assists: number
+  steals: number
+  blocks: number
+  turnovers: number
+  personalFouls: number
+  points: number
+  [key: string]: any
+}
+
+declare interface PlayerStats extends Stats {
+  player: Schema.Types.ObjectId
+}
+
+declare interface TeamStats extends Stats {
+  team: Schema.Types.ObjectId
+}
+
+declare interface GameStats extends Stats {
+  game: Schema.Types.ObjectId
+}
+
 declare interface Player {
   _id: Schema.Types.ObjectId
   apiCode: string
@@ -30,7 +81,8 @@ declare interface Player {
   lastname: string
   team: Schema.Types.ObjectId
   record: string
-  draftSeason: string
+  stats: Schema.Types.ObjectId
+  draftSeason: Schema.Types.ObjectId
   draftRound: number
   draftPick: number
   isDrafted: boolean
@@ -49,9 +101,47 @@ declare interface Coach {
   [key: string]: any
 }
 
-declare interface Year {
+declare interface Team {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  name: string
+  city: string
+  abbreviation: string
+  conference: string
+  division: string
+  record: string
+  stats: Schema.Types.ObjectId
+  [key: string]: any
+}
+
+declare interface Season {
   _id: Schema.Types.ObjectId
   year: Date | string
   apiCode: string
+  [key: string]: any
+}
+
+declare interface DraftSeason {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  year: Date | string
+  [key: string]: any
+}
+
+declare interface DraftPick {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  season: Schema.Types.ObjectId
+  round: number
+  pick: number
+  player: Schema.Types.ObjectId
+  [key: string]: any
+}
+
+declare interface Draft {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  season: Schema.Types.ObjectId
+  picks: Schema.Types.ObjectId[]
   [key: string]: any
 }

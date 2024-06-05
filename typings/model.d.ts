63690f4fdf772536dce0624465ceed85f27e57d6
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-05 23:00:28 +1200

    refactor: Update typings for AllTimeLeaderBoard, AllTimePointsLeaders, AllTimeAssistLeaders, AllTimeReboundLeaders, and Stats interfaces

--- typings/model.d.ts
+++ typings/model.d.ts
@@ -11,14 +11,32 @@ declare global {
 declare interface AllTimeLeaderBoard {
   _id: Schema.Types.ObjectId
   apiCode: string
-  statCategory: string
+  statCategories: Schema.Types.ObjectId[]
+}
+
+declare interface AllTimePointsLeaders {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  players: Schema.Types.ObjectId[]
+  points: number[]
+  rank: number
+  [key: string]: any
 }
 
 declare interface AllTimeAssistLeaders {
   _id: Schema.Types.ObjectId
   apiCode: string
-  player: Schema.Types.ObjectId
-  assist: number
+  players: Schema.Types.ObjectId[]
+  assists: number[]
+  rank: number
+  [key: string]: any
+}
+
+declare interface AllTimeReboundLeaders {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  players: Schema.Types.ObjectId[]
+  rebounds: number[]
   rank: number
   [key: string]: any
 }
@@ -37,29 +55,71 @@ declare interface Game {
   [key: string]: any
 }
 
-declare interface Stats {
+declare interface StatCategory {
   _id: Schema.Types.ObjectId
   apiCode: string
+  name: string
+  [key: string]: any
+}
+
+declare interface Minute {
   minutes: number
+}
+
+declare interface Point extends FieldGoal, ThreePoint, FreeThrow {
+  points: number
+}
+
+declare interface FieldGoal {
   fieldGoalsMade: number
   fieldGoalsAttempted: number
   fieldGoalPercentage: number
+}
+
+declare interface ThreePoint {
   threePointMade: number
   threePointAttempted: number
   threePointPercentage: number
+}
+
+declare interface FreeThrow {
   freeThrowsMade: number
   freeThrowsAttempted: number
   freeThrowPercentage: number
+}
+
+declare interface Rebound {
   offensiveRebounds: number
   defensiveRebounds: number
   totalRebounds: number
+}
+
+declare interface Assist {
   assists: number
+}
+
+declare interface Steal {
   steals: number
+}
+
+declare interface Block {
   blocks: number
+}
+
+declare interface Turnover {
   turnovers: number
+}
+
+declare interface PersonalFoul {
   personalFouls: number
-  points: number
+  technicalFouls: number
+  flagrantFouls: number
   [key: string]: any
+}
+
+declare interface Stats extends Minute, Rebound, Assist, Steal, Block, Turnover, PersonalFoul, Point {
+  _id: Schema.Types.ObjectId
+  apiCode: string
 }
 
 declare interface PlayerStats extends Stats {
@@ -74,6 +134,31 @@ declare interface GameStats extends Stats {
   game: Schema.Types.ObjectId
 }
 
+declare interface RegularSeasonStats extends Stats {
+  season: Schema.Types.ObjectId
+}
+
+declare interface AllStarStats extends Stats {
+  allStar: Schema.Types.ObjectId
+}
+
+declare interface PlayoffStats extends Stats {
+  season: Schema.Types.ObjectId
+}
+
+declare interface FinalsStats extends PlayoffStats {
+  playoffs: Schema.Types.ObjectId
+}
+
+declare interface Playoffs {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  season: Schema.Types.ObjectId
+  games: Schema.Types.ObjectId[]
+  teams: Schema.Types.ObjectId[]
+  [key: string]: any
+}
+
 declare interface Player {
   _id: Schema.Types.ObjectId
   apiCode: string
@@ -82,10 +167,8 @@ declare interface Player {
   team: Schema.Types.ObjectId
   record: string
   stats: Schema.Types.ObjectId
-  draftSeason: Schema.Types.ObjectId
-  draftRound: number
-  draftPick: number
-  isDrafted: boolean
+  draft: Schema.Types.ObjectId
+  jerseys: Schema.Types.ObjectId[]
   isActive: boolean
   [key: string]: any
 }
@@ -111,6 +194,10 @@ declare interface Team {
   division: string
   record: string
   stats: Schema.Types.ObjectId
+  championships: number
+  divisionTitles: number
+  conferenceTitles: number
+  retiredJerseys: Schema.Types.ObjectId[]
   [key: string]: any
 }
 
@@ -125,6 +212,8 @@ declare interface DraftSeason {
   _id: Schema.Types.ObjectId
   apiCode: string
   year: Date | string
+  draft: Schema.Types.ObjectId
+  picks: Schema.Types.ObjectId[]
   [key: string]: any
 }
 
@@ -134,6 +223,7 @@ declare interface DraftPick {
   season: Schema.Types.ObjectId
   round: number
   pick: number
+  team: Schema.Types.ObjectId
   player: Schema.Types.ObjectId
   [key: string]: any
 }
@@ -142,6 +232,96 @@ declare interface Draft {
   _id: Schema.Types.ObjectId
   apiCode: string
   season: Schema.Types.ObjectId
-  picks: Schema.Types.ObjectId[]
+  pick: Schema.Types.ObjectId
+  player: Schema.Types.ObjectId
   [key: string]: any
+}
+
+declare interface AllStarGame {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  season: Schema.Types.ObjectId
+  date: Date
+  location: string
+  mvp: Schema.Types.ObjectId
+  east: Schema.Types.ObjectId
+  west: Schema.Types.ObjectId
+  [key: string]: any
+}
+
+declare interface AllStarEastCoach {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  team: Schema.Types.ObjectId
+  coaches: Schema.Types.ObjectId[]
+}
+
+declare interface AllStarWestCoach {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  team: Schema.Types.ObjectId
+  coaches: Schema.Types.ObjectId[]
+}
+
+declare interface AllStarEast {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  team: Schema.Types.ObjectId
+  players: Schema.Types.ObjectId[]
+  coach: Schema.Types.ObjectId
+}
+
+declare interface AllStarWest {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  team: Schema.Types.ObjectId
+  players: Schema.Types.ObjectId[]
+  coach: Schema.Types.ObjectId
+}
+
+declare interface AllStar {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  game: Schema.Types.ObjectId[]
+  player: Schema.Types.ObjectId
+  selections: number
+  isMVP: boolean
+  isStarter: boolean
+}
+
+declare interface Championship {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  season: Schema.Types.ObjectId
+  team: Schema.Types.ObjectId
+}
+
+declare interface DivisionTitle {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  season: Schema.Types.ObjectId
+  team: Schema.Types.ObjectId
+}
+
+declare interface ConferenceTitle {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  season: Schema.Types.ObjectId
+  team: Schema.Types.ObjectId
+}
+
+declare interface RetiredJersey {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  team: Schema.Types.ObjectId
+  player: Schema.Types.ObjectId
+  jersey: number
+}
+
+declare interface Jersey {
+  _id: Schema.Types.ObjectId
+  apiCode: string
+  team: Schema.Types.ObjectId
+  player: Schema.Types.ObjectId
+  jersey: number
 }

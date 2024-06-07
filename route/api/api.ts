2ea99f2b0e6452a1abbacce5f52214e714a5768a
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-08 02:58:55 +1200

    [Renamed] Remove ApiController class and its dependencies
    This commit removes the ApiController class and its dependencies from the codebase. The class was no longer being used, simplifying the code and reducing unnecessary complexity.

--- route/api/api.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { ApiController } from 'controller'
-
-const router = Router()
-
-router.get('/get', ApiController.getApi)
-
-const apiRoute = router
-export default apiRoute
--- route/api/index.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from 'express'
-import { PATH_DIR } from 'config'
-import apiRoute from 'route/api/api'
-
-const linkApiRoute = (app: Application) => {
-  app.use(PATH_DIR.API, apiRoute)
-}
-
-export { linkApiRoute }

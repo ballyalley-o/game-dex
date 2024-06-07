3420da1d64fb5cd75b39a2bdfa3c299871a9f442
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-08 02:57:38 +1200

    Add SDK path to PATH_DIR configuration

--- config/path-dir.ts
+++ config/path-dir.ts
@@ -3,5 +3,6 @@ import { GLOBAL } from 'config/global'
 import { SEGMENT } from 'constant'
 
 export const PATH_DIR = {
-  API: conNex(SEGMENT.API, GLOBAL.API_VERSION)
+  API: conNex(SEGMENT.API, GLOBAL.API_VERSION),
+  SDK: conNex(SEGMENT.API, GLOBAL.API_VERSION, SEGMENT.SDK)
 }

b2d1a2bd1100b166b639d643facd56693add79a0
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-08 18:38:18 +1200

    refactor: Add HTTP status code enum
    The commit adds a new file `code.ts` that defines an enum representing HTTP status codes. This enum includes codes for informational, success, redirection, client error, server error, and custom error categories. The addition of this enum enhances the codebase by providing a centralized and standardized way to handle HTTP status codes.

---
+++ constant/code.ts
@@ -0,0 +1,68 @@
+/**
+ * Enum representing HTTP status codes.
+ */
+export enum CODE {
+  /**
+   * Informational Codes
+   */
+  CONTINUE = 100,
+  SWITCHING_PROTOCOLS = 101,
+  PROCESSING = 102,
+
+  /**
+   * Success Codes
+   */
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NON_AUTHORITATIVE_INFORMATION = 203,
+  NO_CONTENT = 204,
+  RESET_CONTENT = 205,
+  PARTIAL_CONTENT = 206,
+  MULTI_STATUS = 207,
+  ALREADY_REPORTED = 208,
+  IM_USED = 226,
+
+  /**
+   * Redirection Codes
+   */
+  MULTIPLE_CHOICES = 300,
+  MOVED_PERMANENTLY = 301,
+  FOUND = 302,
+  SEE_OTHER = 303,
+  NOT_MODIFIED = 304,
+  USE_PROXY = 305,
+  TEMPORARY_REDIRECT = 307,
+  PERMANENT_REDIRECT = 308,
+
+  /**
+   * Client Error Codes
+   */
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  METHOD_NOT_ALLOWED = 405,
+  REQUEST_TIMEOUT = 408,
+  CONFLICT = 409,
+  GONE = 410,
+  UNSUPPORTED_MEDIA_TYPE = 415,
+  UNPROCESSABLE_ENTITY = 422,
+  TOO_MANY_REQUESTS = 429,
+
+  /**
+   * Server Error Codes
+   */
+  INTERNAL_SERVER_ERROR = 500,
+  NOT_IMPLEMENTED = 501,
+  BAD_GATEWAY = 502,
+  SERVICE_UNAVAILABLE = 503,
+  GATEWAY_TIMEOUT = 504,
+  HTTP_VERSION_NOT_SUPPORTED = 505,
+
+  /**
+   * Custom Error Codes
+   */
+  CUSTOM_ERROR_1 = 1001,
+  CUSTOM_ERROR_2 = 1002
+}

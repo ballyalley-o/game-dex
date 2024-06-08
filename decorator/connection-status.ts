3343ca68bcd5a3e8320b483f9bddf40d39ce45e6
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-08 18:39:42 +1200

    Refactor decorator functions to include JSDoc comments

--- decorator/connection-status.ts
+++ decorator/connection-status.ts
@@ -1,3 +1,10 @@
+/**
+ * Decorator that sets the connection status of an object.
+ * @param target - The target object.
+ * @param propertyKey - The name of the property being decorated.
+ * @param descriptor - The property descriptor.
+ * @returns The modified property descriptor.
+ */
 function ConnectionStatus(target: IsConnected, propertyKey: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value
 
--- decorator/log-init-request.ts
+++ decorator/log-init-request.ts
@@ -2,6 +2,13 @@ import 'colors'
 import goodlog from 'good-logs'
 import { Request, Response, NextFunction } from 'express'
 
+/**
+ * Decorator function that logs the initialization of a connection request.
+ * @param target - The target object.
+ * @param key - The name of the method being decorated.
+ * @param descriptor - The property descriptor of the method being decorated.
+ * @returns The modified property descriptor.
+ */
 export function LogInitRequest(target: any, key: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value
 

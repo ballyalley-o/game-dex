762af327fb32bccbaa35d9f604be628fa7162382
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-08 22:33:37 +1200

    refactor: Update SDK routes for improved clarity and consistency

--- controller/sdk.ts
+++ controller/sdk.ts
@@ -1,21 +1,39 @@
 import { Request, Response, NextFunction } from 'express'
 import { spawn } from 'child_process'
-import goodlog from 'good-logs'
-import { CODE, KEY, MESSAGE, RESPONSE } from 'constant'
 import axios from 'axios'
+import goodlog from 'good-logs'
 import { SDK_DIR } from 'config/sdk-dir'
+import { CODE, KEY, MESSAGE, RESPONSE } from 'constant'
 
 /**
  * The SDKController class provides methods for interacting with the NBA API.
+ *
+ * @path {base_sdk_url}/sdk
  */
 class SDKController {
-  private static _userId: string
+  private static _playerId: string
+  private static _teamId: string
+  private static _teamAbbv: string
 
-  static setUserId(req: UserRequest) {
-    this._userId = req.user.id
+  static setPlayerId(req: Request) {
+    this._playerId = req.params.id
   }
 
-  public static async getAllTeam(req: Request, res: Response, next: NextFunction) {
+  static setTeamId(req: Request) {
+    this._teamId = req.params.id
+  }
+
+  static setTeamAbbv(req: Request) {
+    this._teamAbbv = req.params.abbv
+  }
+
+  /**
+   * Retrieves all teams.
+   *
+   * @route   {GET} /sdk/team
+   * @access  public
+   */
+  public static async getAllTeam(_req: Request, res: Response, _next: NextFunction) {
     try {
       const teams = await axios.get(SDK_DIR.TEAM_ALL)
 
@@ -30,15 +48,26 @@ class SDKController {
     }
   }
 
-  public static async getTeam(req: Request, res: Response, next: NextFunction) {
+  /**
+   * Retrieves information about a specific team.
+   *
+   * @route   {GET} /sdk/team/:_teamId
+   * @access  public
+   */
+  public static async getTeam(req: Request, res: Response, _next: NextFunction) {
     try {
       const teamId = req.params.id
-      const teams = await axios.get(SDK_DIR.TEAM_INFO(teamId))
 
-      if (!teams.data) {
-        res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      if (!teamId) {
+        res.status(CODE.UNPROCESSABLE_ENTITY).send(RESPONSE.UNPROCESSABLE_ENTITY(MESSAGE.NO_ID))
       } else {
-        res.status(CODE.OK).send(RESPONSE.OK(teams.data))
+        const teams = await axios.get(SDK_DIR.TEAM_INFO(teamId))
+
+        if (!teams.data) {
+          res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+        } else {
+          res.status(CODE.OK).send(RESPONSE.OK(teams.data))
+        }
       }
     } catch (error: any) {
       goodlog.error(error)
@@ -46,15 +75,26 @@ class SDKController {
     }
   }
 
+  /**
+   * Retrieves information about a specific team.
+   *
+   * @route   {GET} /sdk/team/find/:_teamAbbv
+   * @access  public
+   */
   public static async getTeamByAbbv(req: Request, res: Response, next: NextFunction) {
     try {
       const abbv = req.params.abbv
-      const teams = await axios.get(SDK_DIR.TEAM_FIND(abbv))
 
-      if (!teams.data) {
-        res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      if (!abbv) {
+        res.status(CODE.UNPROCESSABLE_ENTITY).send(RESPONSE.UNPROCESSABLE_ENTITY(MESSAGE.NO_ABBV))
       } else {
-        res.status(CODE.OK).send(RESPONSE.OK(teams.data))
+        const team = await axios.get(SDK_DIR.TEAM_FIND(abbv))
+
+        if (!team.data) {
+          res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+        } else {
+          res.status(CODE.OK).send(RESPONSE.OK(team.data))
+        }
       }
     } catch (error: any) {
       goodlog.error(error)
@@ -80,27 +120,82 @@ class SDKController {
   public static async getPlayer(req: Request, res: Response, next: NextFunction) {
     try {
       const playerId = req.params.id
-      const players = await axios.get(SDK_DIR.PLAYER_INFO(playerId))
 
-      res.status(CODE.OK).send(RESPONSE.OK(players.data))
+      if (!playerId) {
+        res.status(CODE.UNPROCESSABLE_ENTITY).send(RESPONSE.UNPROCESSABLE_ENTITY(MESSAGE.NO_ID))
+      } else {
+        const player = await axios.get(SDK_DIR.PLAYER_INFO(playerId))
+
+        if (!player.data) {
+          res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+        } else {
+          res.status(CODE.OK).send(RESPONSE.OK(player.data))
+        }
+      }
     } catch (error: any) {
       goodlog.error(error)
       res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
     }
   }
 
-  public static async getDraftHistory(req: Request, res: Response, next: NextFunction) {
+  public static async getPlayerAward(req: Request, res: Response, next: NextFunction) {
+    try {
+      const playerId = req.params.id
+
+      if (!playerId) {
+        res.status(CODE.UNPROCESSABLE_ENTITY).send(RESPONSE.UNPROCESSABLE_ENTITY(MESSAGE.NO_ID))
+      } else {
+        const playerAward = await axios.get(SDK_DIR.PLAYER_AWARD(playerId))
+
+        if (!playerAward.data) {
+          res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+        } else {
+          res.status(CODE.OK).send(RESPONSE.OK(playerAward.data))
+        }
+      }
+    } catch (error: any) {
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
+    }
+  }
+
+  public static async getPlayerCareer(req: Request, res: Response, next: NextFunction) {
+    try {
+      const playerId = req.params.id
+
+      if (!playerId) {
+        res.status(CODE.UNPROCESSABLE_ENTITY).send(RESPONSE.UNPROCESSABLE_ENTITY(MESSAGE.NO_ID))
+      } else {
+        const playerCareer = await axios.get(SDK_DIR.PLAYER_CAREER(playerId))
+
+        if (!playerCareer.data) {
+          res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+        } else {
+          res.status(CODE.OK).send(RESPONSE.OK(playerCareer.data))
+        }
+      }
+    } catch (error: any) {
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
+    }
+  }
+
+  public static async getDraftHistory(_req: Request, res: Response, _next: NextFunction) {
     try {
       const draftHistory = await axios.get(SDK_DIR.DRAFT_HISTORY)
 
-      res.status(CODE.OK).send(RESPONSE.OK(draftHistory.data))
+      if (!draftHistory.data) {
+        res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      } else {
+        res.status(CODE.OK).send(RESPONSE.OK(draftHistory.data))
+      }
     } catch (error: any) {
       goodlog.error(error)
       res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
     }
   }
 
-  public static async spawnPy(req: Request, res: Response, next: NextFunction) {
+  public static async spawnPy(_req: Request, res: Response, _next: NextFunction) {
     const python = spawn(KEY.PYTHON, ['sdk/server-games.py'])
     let dataToSend: any
 

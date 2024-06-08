f35e9fa1fdc5038173536d271bc838984b34b59d
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-08 18:39:35 +1200

    refactor: Add SDK routes for retrieving team, player, and draft history data
    This commit adds new SDK routes in the `route/sdk` directory. The routes are handled by the `SDKController` and include endpoints for retrieving team, player, and draft history data. This addition enhances the functionality of the application and improves the API's capabilities.

--- controller/sdk.ts
+++ controller/sdk.ts
@@ -1,8 +1,13 @@
 import { Request, Response, NextFunction } from 'express'
 import { spawn } from 'child_process'
 import goodlog from 'good-logs'
+import { CODE, KEY, MESSAGE, RESPONSE } from 'constant'
 import axios from 'axios'
+import { SDK_DIR } from 'config/sdk-dir'
 
+/**
+ * The SDKController class provides methods for interacting with the NBA API.
+ */
 class SDKController {
   private static _userId: string
 
@@ -12,79 +17,102 @@ class SDKController {
 
   public static async getAllTeam(req: Request, res: Response, next: NextFunction) {
     try {
-      const teams = await axios.get('http://127.0.0.1:5001/teams')
+      const teams = await axios.get(SDK_DIR.TEAM_ALL)
 
-      res.status(200).send({
-        success: true,
-        message: 'Teams fetched successfully',
-        data: teams.data
-      })
+      if (!teams.data) {
+        res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      } else {
+        res.status(CODE.OK).send(RESPONSE.OK(teams.data))
+      }
     } catch (error: any) {
-      console.error(error)
-      res.status(500).send({
-        success: false,
-        message: 'Failed to fetch teams',
-        error: error.message
-      })
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
     }
   }
 
   public static async getTeam(req: Request, res: Response, next: NextFunction) {
     try {
       const teamId = req.params.id
-      const teams = await axios.get(`http://127.0.0.1:5001/teams/info/${teamId}`)
+      const teams = await axios.get(SDK_DIR.TEAM_INFO(teamId))
 
-      res.status(200).send({
-        success: true,
-        message: 'Team fetched successfully',
-        data: teams.data
-      })
+      if (!teams.data) {
+        res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      } else {
+        res.status(CODE.OK).send(RESPONSE.OK(teams.data))
+      }
     } catch (error: any) {
-      console.error(error)
-      res.status(500).send({
-        success: false,
-        message: 'Failed to fetch team',
-        error: error.message
-      })
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
+    }
+  }
+
+  public static async getTeamByAbbv(req: Request, res: Response, next: NextFunction) {
+    try {
+      const abbv = req.params.abbv
+      const teams = await axios.get(SDK_DIR.TEAM_FIND(abbv))
+
+      if (!teams.data) {
+        res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      } else {
+        res.status(CODE.OK).send(RESPONSE.OK(teams.data))
+      }
+    } catch (error: any) {
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
     }
   }
 
   public static async getAllPlayer(req: Request, res: Response, next: NextFunction) {
     try {
-      const players = await axios.get('http://127.0.0.1:5001/players')
+      const players = await axios.get(SDK_DIR.PLAYER_ALL)
 
-      res.status(200).send({
-        success: true,
-        message: 'All Players fetched',
-        data: players.data
-      })
+      if (!players.data) {
+        res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      } else {
+        res.status(CODE.OK).send(RESPONSE.OK(players.data))
+      }
     } catch (error: any) {
-      console.error(error)
-      res.status(500).send({
-        success: false,
-        message: 'Failed to fetch all players',
-        error: error.message
-      })
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
+    }
+  }
+
+  public static async getPlayer(req: Request, res: Response, next: NextFunction) {
+    try {
+      const playerId = req.params.id
+      const players = await axios.get(SDK_DIR.PLAYER_INFO(playerId))
+
+      res.status(CODE.OK).send(RESPONSE.OK(players.data))
+    } catch (error: any) {
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
     }
   }
 
   public static async getDraftHistory(req: Request, res: Response, next: NextFunction) {
     try {
-      const draftHistory = await axios.get('http://127.0.0.1:5001/draft/history')
+      const draftHistory = await axios.get(SDK_DIR.DRAFT_HISTORY)
 
-      res.status(200).send({
-        success: true,
-        message: 'Draft History fetched',
-        data: draftHistory.data
-      })
+      res.status(CODE.OK).send(RESPONSE.OK(draftHistory.data))
     } catch (error: any) {
-      console.error(error)
-      res.status(500).send({
-        success: false,
-        message: 'Failed to fetch draft history',
-        error: error.message
-      })
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
     }
+  }
+
+  public static async spawnPy(req: Request, res: Response, next: NextFunction) {
+    const python = spawn(KEY.PYTHON, ['sdk/server-games.py'])
+    let dataToSend: any
+
+    python.stdout.on(KEY.DATA, function (data) {
+      goodlog.log(RESPONSE.ON)
+      dataToSend = data.toString()
+    })
+
+    python.on(KEY.CLOSE, (code) => {
+      goodlog.log(RESPONSE.CLOSE(code))
+      res.status(CODE.OK).json(dataToSend)
+    })
   }
 }
 

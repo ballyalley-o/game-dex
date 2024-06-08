4ced80b8b567ecd294bb3434304d227fe754c15d
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-08 22:27:43 +1200

    documentation: Connect to the database in config/db.ts
    This commit modifies the `config/db.ts` file to connect to the database. The `connectDb` function now takes a boolean parameter `isConnected` to indicate whether the connection is successful. This change improves the clarity and consistency of the codebase.

--- config/db.ts
+++ config/db.ts
@@ -2,6 +2,11 @@ import mongoose from 'mongoose'
 import goodlog from 'good-logs'
 import { GLOBAL } from 'config/global'
 
+/**
+ * Connects to the database.
+ *
+ * @param isConnected - A boolean indicating whether the connection is successful.
+ */
 const connectDb = async (isConnected: boolean) => {
   try {
     const dbConnect = await mongoose.connect(String(GLOBAL.DB_URI))

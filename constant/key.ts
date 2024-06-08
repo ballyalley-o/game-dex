cc8d66541c399c7cd66ceeaa549508cbb117c22b
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-08 18:38:30 +1200

    chore: Add new enum values for KEY

--- constant/key.ts
+++ constant/key.ts
@@ -1,4 +1,7 @@
 export enum KEY {
+  PYTHON = 'python3',
+  DATA = 'data',
+  CLOSE = 'close',
   UNHANDLED_REJECTION = 'unhandledRejection',
   PRODUCTION = 'production',
   DEVELOPMENT = 'development',

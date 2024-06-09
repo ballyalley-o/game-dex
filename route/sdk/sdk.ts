db07ad71ff1e7984d5231457f4feb0daaaea9f49
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-09 22:00:12 +1200

    ```text feat: Add new SDK routes for leaderboards
    This commit adds new SDK routes for leaderboards, including routes for retrieving all leaders, leaders by points (team and player), leaders by assists (team and player), all-time leaders, and all-time totals. These routes enhance the functionality of the SDK module and provide access to valuable leaderboard data.

--- route/sdk/sdk.ts
+++ route/sdk/sdk.ts
@@ -5,13 +5,25 @@ import { PATH_SDK } from 'config'
 const router = Router()
 
 router.get(PATH_SDK.TEAM, SDKController.getAllTeam)
+router.get(PATH_SDK.TEAM_STATE, SDKController.getAllTeamByState)
 router.get(PATH_SDK.TEAM_ID, SDKController.getTeam)
 router.get(PATH_SDK.TEAM_ABBV, SDKController.getTeamByAbbv)
+
 router.get(PATH_SDK.PLAYER, SDKController.getAllPlayer)
 router.get(PATH_SDK.PLAYER_ID, SDKController.getPlayer)
 router.get(PATH_SDK.PLAYER_AWARD, SDKController.getPlayerAward)
 router.get(PATH_SDK.PLAYER_CAREER, SDKController.getPlayerCareer)
+
 router.get(PATH_SDK.DRAFT_HISTORY, SDKController.getDraftHistory)
+
+router.get(PATH_SDK.LEADER_ALL, SDKController.getAllLeader)
+router.get(PATH_SDK.LEADER_PT, SDKController.getLeaderPtTeam)
+router.get(PATH_SDK.LEADER_PT_PLAYER, SDKController.getLeaderPtPlayer)
+router.get(PATH_SDK.LEADER_AST, SDKController.getLeaderAstTeam)
+router.get(PATH_SDK.LEADER_AST_PLAYER, SDKController.getLeaderAstPlayer)
+
+router.get(PATH_SDK.ALL_TIME_LEADER, SDKController.getAllTimeLeader)
+router.get(PATH_SDK.ALL_TIME_TOTAL, SDKController.getAllTimeTotal)
 
 /**
  * Route for SDK module

8c2e9b44e9b84fc3a0862f9db67f904ee604d292
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-09 22:00:38 +1200

    Update SDK directory paths

--- sdk.sh
+++ sdk.sh
@@ -2,7 +2,9 @@
 
 # python3 scraper/server-stats.py
 # python3 scraper/server-players.py
-python3 sdk/server-players-teams.py
+# python3 sdk/server-players-teams.py
+
+python3 sdk/sdk-server.py
 
 # Path: scrape.sh
 #!/bin/bash

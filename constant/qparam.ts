23ace5145f3eb468eac3e24090ee7c2e6942abc2
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-11 22:40:42 +1200

    added params, and merged CONFERENCE and PERIOD to QPARAMS

--- constant/qparam.ts
+++ constant/qparam.ts
@@ -51,7 +51,100 @@ export const ALL_TIME_LEADER_GRID = {
   TOP_X: 10
 }
 
+export const LEAGUE_ID = {
+  nba: '00',
+  aba: '01',
+  wnba: '10',
+  g_league: '20'
+}
+
+export const PERIOD = {
+  ALL: '0',
+  Q1: '1',
+  Q2: '2',
+  Q3: '3',
+  Q4: '4',
+  OT: '5'
+}
+
+export const OUTCOME = {
+  WIN: 'W',
+  LOSS: 'L'
+}
+
+export const PER_MODE_SIMPLE = {
+  PER_GAME: 'PerGame',
+  TOTAL: 'Totals'
+}
+
+export const PER_MODE_36 = {
+  PER_36: 'Per36'
+}
+
+export const PER_MODE_48 = {
+  PER_48: 'Per48'
+}
+
+export const PER_MODE_TIME = {
+  PER_MINUTE: 'MinutesPer',
+  PER_40: 'Per40'
+}
+
+export const PER_MODE_DETAILED = {
+  PER_MINUTE: 'MinutesPer',
+  PER_POSSESSION: 'PerPossession',
+  PER_PLAY: 'PerPlay',
+  PER_100: 'Per100Possessions',
+  PER_100_PLAY: 'Per100Plays'
+}
+
+export const PER = {
+  ...{ PER_MODE_SIMPLE },
+  ...{ PER_MODE_36 },
+  ...{ PER_MODE_48 },
+  ...{ PER_MODE_TIME }
+}
+
+export const PLAYER_EXPERIENCE = {
+  ROOKIE: 'Rookie',
+  SOPHOMORE: 'Sophomore',
+  VETERAN: 'Veteran'
+}
+
+export const PLAYER_POSITION = {
+  GUARD: 'Guard',
+  FORWARD: 'Forward',
+  CENTER: 'Center'
+}
+
+export const PLAYER_POSITION_ABBV = {
+  G: 'G',
+  F: 'F',
+  C: 'C',
+  C_F: 'C-F',
+  F_C: 'F-C',
+  F_G: 'F-G',
+  G_F: 'G-F'
+}
+
+export const CLUTCH_TIME = {
+  LAST_5_MINUTES: 'Last 5 Minutes',
+  LAST_3_MINUTES: 'Last 3 Minutes',
+  LAST_1_MINUTE: 'Last 1 Minute',
+  LAST_30_SECONDS: 'Last 30 Seconds',
+  LAST_10_SECONDS: 'Last 10 Seconds'
+}
+
 export const QPARAM = {
+  ...CLUTCH_TIME,
+  ...PER,
+  ...PLAYER_EXPERIENCE,
+  ...PLAYER_POSITION,
+  ...PLAYER_POSITION_ABBV,
+  ...OUTCOME,
+  ...PERIOD,
+  ...LEAGUE_ID,
+  ...ALL_TIME_LEADER_GRID,
   ...FRANCHISE_LEADER,
   ...PLAYER_OR_TEAM,
   ...SCOPE,

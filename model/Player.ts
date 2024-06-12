bc2835819d55ba1bce86e950cf71e6c5032d1a71
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-13 02:29:57 +1200

    feat: Add high school field to Player model

--- model/Player.ts
+++ model/Player.ts
@@ -1,20 +1,63 @@
 import slugify from 'slugify'
 import goodlog from 'good-logs'
 import mongoose, { Schema, model } from 'mongoose'
+import { TAGS } from 'constant'
 
-const TAG = 'Player'
+const TAG = TAGS.PLAYER
 
-const PlayerSchema: Schema<Player> = new Schema<Player>({
-  apiCode: { type: String, required: true },
-  firstname: { type: String, required: true },
-  lastname: { type: String, required: true },
-  fullname: { type: String },
-  birthDate: { type: Date },
-  birthPlace: { type: String },
-  highSchool: { type: String },
-  college: { type: String },
-  height: { type: String },
-  weight: { type: Number },
-  position: { type: [String] },
-  nickname: { type: [String] }
+const PlayerSchema: Schema<Player> = new Schema<Player>(
+  {
+    apiCode: { type: String, required: true, unique: true },
+    playerCode: { type: String, unique: true },
+    firstname: { type: String },
+    lastname: { type: String },
+    nickname: { type: [String] },
+    birthDate: { type: Date },
+    birthPlace: { type: String },
+    highSchool: { type: String },
+    college: { type: String },
+    height: { type: String },
+    weight: { type: Number },
+    position: { type: [String] },
+    draft: { type: Schema.Types.ObjectId, ref: 'Draft' },
+    team: { type: [Schema.Types.ObjectId], ref: 'Team' },
+    stats: { type: Schema.Types.ObjectId, ref: 'PlayerStats' },
+    allStar: { type: Schema.Types.ObjectId, ref: 'AllStar' },
+    awards: { type: [Schema.Types.ObjectId], ref: 'Award' },
+    jerseys: { type: [Schema.Types.ObjectId], ref: 'Jersey' },
+    leagues: { type: [Schema.Types.ObjectId], ref: 'League' },
+    slug: { type: [String] },
+    isActive: { type: Boolean, default: true }
+  },
+  {
+    timestamps: true,
+    collation: { locale: 'en', strength: 2 },
+    collection: TAG,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+)
+
+PlayerSchema.virtual('fullname').get(function () {
+  return `${this.firstname} ${this.lastname}`
 })
+
+PlayerSchema.virtual('age').get(function () {
+  const diff = Date.now() - this.birthDate.getTime()
+  const ageDate = new Date(diff)
+  return Math.abs(ageDate.getUTCFullYear() - 1970)
+})
+
+PlayerSchema.index({ apiCode: 1 }, { unique: true })
+
+PlayerSchema.pre('save', function (next) {
+  goodlog.log(`${TAG} pre save hook`)
+  next()
+})
+
+PlayerSchema.post('save', function (doc) {
+  goodlog.log(`${TAG} post save hook`)
+})
+
+const Player = model<Player>(TAG, PlayerSchema)
+export default Player

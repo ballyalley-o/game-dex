a8cd1c07466b96d077ea204ce4baf61c7561a3a7
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-14 00:21:14 +1200

    League Controller #10: Add getAllLeague, updateLeague, and deleteLeague methods to LeagueController
    This commit adds the `getAllLeague`, `updateLeague`, and `deleteLeague` methods to the `LeagueController` class in the `league.ts` file. These methods provide functionality for retrieving all leagues, updating a league, and deleting a league respectively. This addition enhances the functionality of the application by providing a centralized and standardized way to handle league-related operations.
    
    Co-authored-by: [Author Name]

--- controller/league.ts
+++ controller/league.ts
@@ -6,13 +6,26 @@ import { SDK_DIR } from 'config/sdk-dir'
 import { CODE, KEY, MESSAGE, RESPONSE, QPARAM } from 'constant'
 
 class LeagueController {
+  public static async getAllLeague(req: Request, res: Response, next: NextFunction) {
+    try {
+      const leagues = await League.find()
+      if (!leagues) {
+        return res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      }
+      res.status(CODE.OK).send(RESPONSE.OK(leagues))
+    } catch (error: any) {
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
+    }
+  }
+
   public static async createLeague(req: Request, res: Response, next: NextFunction) {
     try {
       const { apiCode, league, commissioner, founded, teams, headquarters, website, logo } = req.body
 
       const leagueExists = await League.findOne({ apiCode })
       if (leagueExists) {
-        return res.status(CODE.CONFLICT).send(RESPONSE.CONFLICT('League already exists'))
+        return res.status(CODE.CONFLICT).send(RESPONSE.CONFLICT(MESSAGE.ALREADY_EXISTS))
       }
 
       const leagueData = {
@@ -29,9 +42,42 @@ class LeagueController {
       const newLeague = await League.create(leagueData)
 
       if (!newLeague) {
-        return res.status(CODE.BAD_REQUEST).send(RESPONSE.BAD_REQUEST('Failed to create league'))
+        return res.status(CODE.BAD_REQUEST).send(RESPONSE.BAD_REQUEST(MESSAGE.FAILED_CREATE))
       }
       res.status(CODE.CREATED).send(RESPONSE.CREATED(newLeague))
+    } catch (error: any) {
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
+    }
+  }
+
+  public static async updateLeague(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { apiCode } = req.params
+      const leagueData = req.body
+
+      const league = await League.findOneAndUpdate({ apiCode }, leagueData, { new: true })
+
+      if (!league) {
+        return res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      }
+      res.status(CODE.OK).send(RESPONSE.OK(league))
+    } catch (error: any) {
+      goodlog.error(error)
+      res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))
+    }
+  }
+
+  public static async deleteLeague(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { apiCode } = req.params
+
+      const league = await League.findOneAndDelete({ apiCode })
+
+      if (!league) {
+        return res.status(CODE.NOT_FOUND).send(RESPONSE.NOT_FOUND(MESSAGE.NOT_FOUND))
+      }
+      res.status(CODE.OK).send(RESPONSE.OK(league))
     } catch (error: any) {
       goodlog.error(error)
       res.status(CODE.INTERNAL_SERVER_ERROR).send(RESPONSE.INTERNAL_SERVER_ERROR(error.message))

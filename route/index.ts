cce870e83183068a74b5d2cbde2b335721745d80
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-15 21:27:23 +1200

    feat: Add franchise route and controller for creating franchise history

--- route/index.ts
+++ route/index.ts
@@ -1,10 +1,12 @@
 import { Application } from 'express'
 import { linkSdkRoute } from 'route/sdk'
 import { linkLeagueRoute } from 'route/league'
+import { linkFranchiseRoute } from 'route/franchise'
 
 const mainRoute = (app: Application) => {
   linkSdkRoute(app)
   linkLeagueRoute(app)
+  linkFranchiseRoute(app)
 }
 
 export { mainRoute }

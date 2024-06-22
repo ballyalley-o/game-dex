7e3e2380fa4ceed507ea42404beccc241fdfa3fb
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-22 23:59:50 +1200

    feat: Add PlayerController to controller/index.ts
    This commit adds the PlayerController to the controller/index.ts file. The PlayerController is responsible for handling player-related functionality. This addition enhances the functionality of the application by providing a convenient way to manage player data.

--- controller/index.ts
+++ controller/index.ts
@@ -1,3 +1,4 @@
 export { default as SDKController } from './sdk'
 export { default as SDKSetController } from './sdk-set'
 export { default as LeagueController } from './league'
+export { default as PlayerController } from './player'

d238300325bb2749081e83ab814ef080a1e2bbef
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-23 00:04:10 +1200

    Team Module #31: Add Team model and schema
    This commit adds the Team model and schema to the project. The Team model represents a sports team and includes fields for the team's API code, name, city, abbreviation, nickname, season, coach, stats, stats history, players, and franchise. The schema defines the structure and validation rules for the Team model. This addition enhances the functionality of the application by providing a way to store and retrieve team data.

---
+++ model/Team.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from 'mongoose'
+import { TAGS } from 'constant'
+import goodlog from 'good-logs'
+
+const TAG = TAGS.TEAM
+
+const TeamSchema: Schema<Team> = new Schema<Team>(
+  {
+    apiCode: { type: String, required: true },
+    name: { type: String, required: true },
+    city: { type: String, required: true },
+    abbreviation: { type: String, required: true },
+    nickname: { type: String },
+    season: { type: String },
+    coach: { type: String },
+    stats: { type: Schema.Types.ObjectId, ref: 'TeamStats' },
+    statsHistory: [{ type: Schema.Types.ObjectId, ref: 'TeamStats' }],
+    players: [{ type: Schema.Types.ObjectId, ref: 'Player' }],
+    franchise: { type: Schema.Types.ObjectId, ref: 'Franchise' }
+  },
+  {
+    timestamps: true,
+    collation: { locale: 'en', strength: 2 },
+    collection: TAG,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+)
+
+TeamSchema.virtual('slug').get(function () {
+  return this.name.toLowerCase().replace(/\s/g, '-')
+})
+
+TeamSchema.pre('save', function (next) {
+  goodlog.log(`${TAGS.TEAM} pre save hook`)
+  next()
+})
+
+TeamSchema.post('save', function (doc) {
+  goodlog.log(`${TAGS.TEAM} post save hook`)
+})
+
+TeamSchema.index({ name: 1 }, { unique: true })
+
+const Team = model<Team>(TAG, TeamSchema)
+export default Team

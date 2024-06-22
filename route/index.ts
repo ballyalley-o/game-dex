f0940e02b53b1ccbaf1fc62a78fd55aa301ac9e8
ballyalley-o <bally.lomibao@howickltd.com>
2024-06-23 00:05:26 +1200

    Add player and team routes and controllers, moved player to folder

--- route/index.ts
+++ route/index.ts
@@ -2,11 +2,15 @@ import { Application } from 'express'
 import { linkSdkRoute } from 'route/sdk'
 import { linkLeagueRoute } from 'route/league'
 import { linkFranchiseRoute } from 'route/franchise'
+import { linkPlayerRoute } from './player'
+import { linkTeamRoute } from './team'
 
 const mainRoute = (app: Application) => {
   linkSdkRoute(app)
   linkLeagueRoute(app)
   linkFranchiseRoute(app)
+  linkTeamRoute(app)
+  linkPlayerRoute(app)
 }
 
 export { mainRoute }
